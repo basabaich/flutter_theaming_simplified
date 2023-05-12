4ff60aaf3cc1decf94b95bd3ae1ce1d515883532
Basab Aich <basabaich@hotmail.com>
2023-05-12 23:19:39 +0530

    First commit

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -34,6 +34,25 @@ class _NewExpenseState extends State<NewExpense> {
   final _amountController = TextEditingController();
   //
   //
+  void _presentDatePicker() {
+    //We create a variable to create the "initialDate", "firstDate" &
+    //"lastDate"
+    final now = DateTime.now();
+    //
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+    //"ShowDatePicker" is a built in function in flutter.
+  }
+
+  //We can put this method which will take the date time picker icon pressed
+  //event, before or after "@override" as it doesn't matter.
+  //
+  //
   @override
   //We need to dispose the "TextEditingController()" once the modal bottom
   //sheet is closed. So teh following procedure is followed if we call
@@ -76,20 +95,57 @@ class _NewExpenseState extends State<NewExpense> {
           ), //TextField
           //
           //
-          //Similarly we create another textfield for amount
-          TextField(
-            controller: _amountController,
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              label: Text('Amount'),
-              //
-              //
-              prefixText: 'Rs. ',
-              //"PrefixText:" puts a currency notation the moment we click
-              // on "amount" text field to enter an amount. No backslash is
-              // necessary.
-            ), //InputDecoration
-          ), //TextField
+          //
+          //we now put up a "Row()" here so that we can reduce the size of the
+          //amount text field and put a "DateTime()" input function there.
+          //Also, we put the second Text field inside the row's first widget
+          //of the children. teh second widget will be the DateTime widget.
+          Row(
+            children: [
+              //Similarly we create another textfield for amount
+              Expanded(
+                //As "TextField" have problem in rendering within Rows , so
+                //we refactor this "TextField" with widget "Expanded"
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    label: Text('Amount'),
+                    //
+                    //
+                    prefixText: 'Rs. ',
+                    //"PrefixText:" puts a currency notation the moment we click
+                    // on "amount" text field to enter an amount. No backslash is
+                    // necessary.
+                  ), //InputDecoration
+                ), //TextField
+              ), //Expanded
+              const SizedBox(
+                width: 16,
+              ), //SizedBox
+              Expanded(
+                //As two Rows can not be within one  another, so we refactor
+                //this Row with widget "Expanded"
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  //This will push the next widget to the extreme right of the
+                  //Screen, i.e. the end of the screen.
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  //This will keep the next widget at the centre horizontally.
+                  children: [
+                    const Text('Selected date'),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      //This method is used as a pointer for teh "onPressed()"
+                      //function.
+                      icon: const Icon(Icons.calendar_month),
+                    ), //IconButton
+                  ],
+                ), //Row
+              ), //Expanded
+            ],
+          ), //Row
+
           const SizedBox(
             width: 16,
           ), //SizedBox
